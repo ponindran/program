bff188157aae610ffd1e61025a6ab3233967f35b
Selvakumar <selvaarun22400@gmail.com>
2021-11-17 13:40:59 +0530

    CRUD Operation Of Employee

---
+++ Web.Api/Entity/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Api.Entity
+{
+    /// <summary>
+    /// Is used to map the table "employee" in the database
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+        public string Email { get; set; }
+    }
+}
