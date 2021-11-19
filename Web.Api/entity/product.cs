98f66019ab008705d6f5a15f29262e477dd0023a
ponindran <ponindran1212@gmail.com>
2021-11-19 14:51:38 +0530

    product

---
+++ Web.Api/entity/product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Api.Entity
+{
+    public class Table
+    {
+        public int Id { get; set; }
+
+        public string productName { get; set; }
+
+        public int price { get; set; }
+  
+    }
+}
