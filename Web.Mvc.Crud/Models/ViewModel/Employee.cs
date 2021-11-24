1460b712d37f6e77c60b8e19c287e324b9ccefdc
Sureshkumar Duraisamy <sureshkumar.duraisamy@virginactive.co.uk>
2021-11-24 16:58:50 +0000

    Adding Sample Crud OPeration

---
+++ Web.Mvc.Crud/Models/ViewModel/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Mvc.Crud.Models.ViewModel
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoiningDate { get; set; }
+    }
+}
--- Web.Mvc.Crud/Startup.cs
+++ Web.Mvc.Crud/Startup.cs
@@ -50,7 +50,7 @@ namespace Web.Mvc.Crud
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Employee}/{action=Index}/{id?}");
             });
         }
     }
