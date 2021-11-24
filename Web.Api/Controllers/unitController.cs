4e2500a7a70d58c58d517a3816b7e61364d266d6
Selvakumar <selvaarun22400@gmail.com>
2021-11-24 16:46:49 +0530

    crud complete

--- Web.Api/Controllers/unitController.cs
+++ Web.Api/Controllers/unitController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Web.Api.BusinessService;
+using Web.Api.Repository;
 
 namespace Web.Api.Controllers
 {
@@ -15,18 +16,48 @@ namespace Web.Api.Controllers
 
     {
         private readonly IScaleService _sclService;
+        private readonly IScalerepository _sclRepository;
 
-        public unitController(IScaleService sclService)
+        public unitController(IScaleService sclService, IScalerepository sclRepository)
         {
             _sclService = sclService;
+            _sclRepository = sclRepository;
         }
 
-        [HttpPost("Scale/QuantityDetails")]
-        public void PostQuantityDetails([FromBody] Scale sclDTO)
+        
+        [HttpGet("get/Quantity")]
+        public IActionResult getQuantity() 
         {
+           
+            return Ok( _sclRepository.SelectALLQuantity());
 
-
-            _sclService.InsertData(sclDTO);
         }
+        [HttpPost("Scale/QuantityDetails")]
+        public IActionResult PostQuantityDetails([FromBody] Scale sclDTO)
+        {
+            try
+            {
+
+                _sclService.InsertData(sclDTO);
+                return Ok("test");
+            }
+            catch (Exception ex) 
+            {
+                return Unauthorized(ex);
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]  Web.Api.Entity.Scale sclEntity)
+        {
+           return Ok ( _sclRepository.UpdateData(id,sclEntity));
+             
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _sclRepository.DeleteData(id);
+        }
+
     }
 }
